8605312d8fa244a5cafad2b55115e47ed3253ba7
ParetoOptimalDev <pareto.optimal@mailfence.com>
2022-08-18 15:50:37 -0500

    working nix-mk-shell-bin example

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
-# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 {
-  description = "My haskell application";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    msb.url = github:rrbutani/nix-mk-shell-bin;
+    nixpkgs.url = github:nixOS/nixpkgs/22.05;
+    flu.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { msb, nixpkgs, flu, ... }: with msb.lib; with flu.lib; eachDefaultSystem(system: let
+    np = import nixpkgs { inherit system; };
 
-        haskellPackages = pkgs.haskellPackages;
+    # Like `nix-shell`, this will build the dependencies of `pkg` but not
+    # `pkg` itself.
+    pkg = np.hello;
+    pkgShellBin = mkShellBin { drv = pkg; nixpkgs = np; };
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+    # Here, `shellBin` *will* build `pkg`. This is like `nix develop`.
+    shell = np.mkShell { name = "example"; nativeBuildInputs = [pkg]; };
+    shellBin = msb.lib.mkShellBin { drv = shell; nixpkgs = np; bashPrompt = "[hello]$ "; };
 
-        packageName = "hello";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
+  in {
+    # You can run `nix bundle` and get a self-contained executable that,
+    # when run, drops you into a shell containing `pkg`.
+    packages.default = shellBin;
+    packages.pkgShellBin = pkgShellBin;
 
-        defaultPackage = self.packages.${system}.${packageName};
+    # You can run the derivations `mkShellBin` produces:
+    apps.default = { type = "app"; program = "${shellBin}/bin/${shellBin.name}"; };
 
-        devShell = pkgs.mkShell {
-          shellHook = "runHook setupCompilerEnvironmentPhase";
-          buildInputs = with pkgs; [
-            cabal-install
-
-            # this isn't enough to get `aeson` in the environment
-            # maybe if we somehow say to also run
-            # packages.x86_64-linux.hello.setupCompilerEnvironmentPhase
-            # then things will work
-            self.packages.${system}.${packageName}
-          ];
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+    # The above is more or less equivalent to:
+    devShells.default = shell;
+  });
 }
