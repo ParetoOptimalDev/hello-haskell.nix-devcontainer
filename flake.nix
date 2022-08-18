d8f266d0764b9c1f798b443315adaa724524a9cb
ParetoOptimalDev <pareto.optimal@mailfence.com>
2022-08-18 16:14:18 -0500

    add my haskell package and try to build docker container

--- flake.nix
+++ flake.nix
@@ -1,3 +1,8 @@
+# success criteria: This command returns a matching line indicating that aeson was loaded into the ghc package database
+# nix build .#devContainerImage && docker load < result && docker run -v $PWD:/workspace/hello --rm -it hello-nix-devcontainer bash -c "ghc-pkg list | grep aeson"
+# currently returns: bash: line 1: ghc-pkg: command not found
+
+
 {
   inputs = {
     msb.url = github:rrbutani/nix-mk-shell-bin;
@@ -5,23 +10,41 @@
     flu.url = github:numtide/flake-utils;
   };
 
-  outputs = { msb, nixpkgs, flu, ... }: with msb.lib; with flu.lib; eachDefaultSystem(system: let
+  outputs = { self, msb, nixpkgs, flu, ... }: with msb.lib; with flu.lib; eachDefaultSystem(system: let
     np = import nixpkgs { inherit system; };
 
+    haskellPackages = np.haskellPackages;
+    packageName = "hello";
+    
     # Like `nix-shell`, this will build the dependencies of `pkg` but not
     # `pkg` itself.
-    pkg = np.hello;
+    pkg = haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+          };
     pkgShellBin = mkShellBin { drv = pkg; nixpkgs = np; };
 
     # Here, `shellBin` *will* build `pkg`. This is like `nix develop`.
     shell = np.mkShell { name = "example"; nativeBuildInputs = [pkg]; };
-    shellBin = msb.lib.mkShellBin { drv = shell; nixpkgs = np; bashPrompt = "[hello]$ "; };
+    shellBin = msb.lib.mkShellBin { drv = shell; nixpkgs = np; bashPrompt = "[hithere]$ "; };
 
   in {
     # You can run `nix bundle` and get a self-contained executable that,
     # when run, drops you into a shell containing `pkg`.
     packages.default = shellBin;
     packages.pkgShellBin = pkgShellBin;
+
+    packages.devContainerImage = np.dockerTools.buildLayeredImage {
+        name = "hello-nix-devcontainer";
+        tag = "latest";
+        # extraCommands = ''
+        #   #!${np.runtimeShell}
+        # '';
+        contents = with np; [
+          bash coreutils cacert tzdata fd git  busybox
+          shellBin
+          pkgShellBin
+        ];
+      };
 
     # You can run the derivations `mkShellBin` produces:
     apps.default = { type = "app"; program = "${shellBin}/bin/${shellBin.name}"; };
